c3554f7cdfebad25dcd681a86408f17baeeaa6de
HARISH BALAKRISHNAN <72798342+HARISH-B-2001@users.noreply.github.com>
2025-03-19 17:50:56 +0530

    Update gitprofile.tsx

--- src/components/gitprofile.tsx
+++ src/components/gitprofile.tsx
@@ -24,6 +24,7 @@ import SkillCard from './skill-card';
 import ExperienceCard from './experience-card';
 import EducationCard from './education-card';
 import CertificationCard from './certification-card';
+import AchivementCard from './achivement-card';
 import { GithubProject } from '../interfaces/github-project';
 import GithubProjectCard from './github-project-card';
 import ExternalProjectCard from './external-project-card';
